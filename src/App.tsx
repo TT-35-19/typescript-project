56b371caf9c0d602a50ef08295890c43bc43dd74
TaiyoshiTakahiro <popo0129tt35@gmail.com>
2022-03-22 07:19:51 +0900

    todoをリストで追加できるよう実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // 新しいTodoを作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  }
+
   return (
     <div className="App">
       <div>
         <h2>ToDoリスト with Typescript</h2>
-        <form onSubmit={() => { }}>
-          <input type="text"
-            onChange={() => { }}
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            type="text"
+            onChange={(e) => handleChange(e)}
             className="inputText"
           />
           <input type="submit" value="作成" className="submitButton" />
         </form>
+        <ul className="todoList">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.inputValue}
+            </li>
+          ))}s
+        </ul>
       </div>
     </div>
   );

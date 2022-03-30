8870ffef67e06724175d3ffb491c410e81f3e47a
TaiyoshiTakahiro <popo0129tt35@gmail.com>
2022-03-30 23:16:20 +0900

    todoリスト実装

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,34 @@ function App() {
 
     setTodos([newTodo, ...todos]);
     setInputValue("");
-  }
+  };
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  const handleChecked = (id: number, checked: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !checked;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  const handleDelete = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
@@ -44,9 +71,20 @@ function App() {
         <ul className="todoList">
           {todos.map((todo) => (
             <li key={todo.id}>
-              {todo.inputValue}
+              <input
+                type="text"
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+                className="inputText"
+                value={todo.inputValue}
+                disabled={todo.checked}
+              />
+              <input
+                type="checkbox"
+                onChange={(e) => handleChecked(todo.id, todo.checked)}
+              />
+              <button onClick={() => handleDelete(todo.id)}>消</button>
             </li>
-          ))}s
+          ))}
         </ul>
       </div>
     </div>
